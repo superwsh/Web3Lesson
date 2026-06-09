8d59c1f5b5f08912b2e1036456773ad77ef44e41
superwsh <1316027963@qq.com>
2026-06-09 17:26:47 +0800

    调用父构造函数

--- solidity基础/继承is.sol
+++ solidity基础/继承is.sol
@@ -65,3 +65,46 @@ contract C is A,B{
         return "C";
     }
 }
+
+// 【调用父合约构造函数】编程作业：
+// 1. 声明父合约：
+// ◦ 编写一个名为 S 的合约，包含一个字符串类型的状态变量 name 和一个接收 name 参数的构造函数。
+// ◦ 编写一个名为 T 的合约，包含一个字符串类型的状态变量 text 和一个接收 text 参数的构造函数。
+// 2. 创建继承合约：
+// ◦ 编写一个名为 U 的合约，继承 S 和 T 。
+// ◦ 在 U 的构造函数中调用 S 和 T 的构造函数，并传递相应的参数。
+// 3. 混合构造函数参数：
+// ◦ 编写一个名为 BB 的合约，继承 S 和 T 。
+// ◦ 静态传递 S 的构造函数参数，动态传递 T 的构造函数参数。
+// 4. 验证初始化顺序：
+// ◦ 编写一个名为 B0 的合约，先继承 S 再继承 T ，验证构造函数的调用顺序。
+// ◦ 编写一个名为 B2 的合约，先继承 T 再继承 S ，验证构造函数的调用顺序。
+
+contract S {
+    string public name;
+    constructor(string memory _name){
+        name = _name;
+    }
+}
+contract T {
+    string public tet;
+    constructor(string memory _tet){
+        tet = _tet;
+    }
+}
+contract U is S,T{
+    constructor(string memory _name,string memory _text) S(_name) T(_text) {}
+}
+
+contract BB is S("staticName"),T{
+    constructor(string memory _text) T(_text) {}
+}
+
+// 初始化顺序由继承的顺序决定，而非调用顺序
+contract B0 is S,T{
+    constructor(string memory _name,string memory _text) S(_name) T(_text) {}
+}
+
+contract B1 is T,S{
+    constructor(string memory _name,string memory _text) S(_name) T(_text) {}
+}
