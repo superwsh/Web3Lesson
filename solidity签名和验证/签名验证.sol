b945cb46c96c95fd7d5ea13e1b1182aed145d89a
superwsh <1316027963@qq.com>
2026-06-12 20:57:53 +0800

    解释二次签名原因

--- solidity签名和验证/签名验证.sol
+++ solidity签名和验证/签名验证.sol
@@ -18,7 +18,7 @@ contract SignatureVerify {
 
     // 3. getEthSignedMessageHash 函数，用于生成Ethereum签名消息哈希。
     function getEthSignedMessageHash(bytes32 messageHash) public pure returns(bytes32) {
-        return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash));
+        return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash));//二次加工，这个签名表名它不是一个交易签名
     }
 
     // 4. recover 函数，用于恢复签名者地址。
