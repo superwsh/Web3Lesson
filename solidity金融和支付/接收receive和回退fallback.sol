0036355dbbfd0f21b3901fd47657156e2ea9b5db
superwsh <1316027963@qq.com>
2026-06-21 15:21:54 +0800

    fallback函数输入和输出

--- solidity金融和支付/接收receive和回退fallback.sol
+++ solidity金融和支付/接收receive和回退fallback.sol
@@ -26,3 +26,49 @@ contract Fallback {
         emit Log("receive",msg.sender, msg.value, "");
     }
 }
+
+
+// 1. 计数器合约
+// ◦ 声明一个 count 状态变量
+// ◦ 编写一个 inc 函数，每次调用时将 count 加1，并返回 count 的当前值
+// 2. 回退输入输出合约
+// ◦ 编写一个回退函数，能够接受 bytes 类型的数据并返回 bytes 类型的数据
+// ◦ 回退函数内部调用计数器合约的 inc 函数，并返回调用结果
+// 3. 测试合约
+// ◦ 编写一个测试函数，接受回退合约的地址和数据，调用回退函数，并返回结果
+// ◦ 编写一个辅助函数，生成调用计数器合约 inc 函数的数据
+
+contract Counter {
+    uint internal count;
+    function inc() external returns(uint) {
+        count += 1;
+        return count;
+    }
+}
+
+contract FallbackIo {
+    address addr;
+    constructor(address _addr) {
+        addr = _addr;
+    }
+    fallback(bytes calldata funcBytes) external payable returns(bytes memory) {
+         (bool success,bytes memory result) = addr.call(funcBytes);
+         require(success, "call failed");
+         return result;
+    }
+}
+
+contract TestContract {
+    event Log(bytes res);
+
+    function test(address _fallback,bytes calldata funcBytes ) external {
+        (bool success,bytes memory result)  = _fallback.call(funcBytes);
+        require(success, "callfailed");
+        emit Log(result);
+    }
+
+    function getFuncBytes() external pure returns(bytes memory) {
+        return abi.encodeWithSignature("inc()");
+    }
+}
+
